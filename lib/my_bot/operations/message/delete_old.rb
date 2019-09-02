1794e01065bc4b49b488207900515bf510adc920
vrtsev <v.rtsev@gmail.com>
2019-09-02 21:04:06 +0300

    MyBot Sample Operations

---
+++ lib/my_bot/operations/message/delete_old.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# module MyBot
+#   module Op
+#     module Message
+#       class DeleteOld < Telegram::AppManager::BaseOperation
+
+#         step :delete_messages
+
+#         def delete_messages(ctx, **)
+#           MyBot::MessageRepository.new.delete_old
+#         end
+
+#       end
+#     end
+#   end
+# end
