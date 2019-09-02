a60e883a31d8501ce085de37e954c34cbd177374
vrtsev <v.rtsev@gmail.com>
2019-09-02 21:03:57 +0300

    MyBot Sample workers

---
+++ lib/my_bot/workers/delete_old_messages_worker.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# module MyBot
+#   class DeleteOldMessagesWorker < Telegram::AppManager::BaseWorker
+
+#     def perform
+#       result = MyBot::Op::Message::DeleteOld.call
+#       handle_operation_errors(result[:error]) if result.failure?
+#     end
+
+#   end
+# end
