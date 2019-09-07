7f1c9d23dba4190f3d1e90fe4612dc02b1ecfe4e
vrtsev <v.rtsev@gmail.com>
2019-09-07 21:17:20 +0000

    Fixed migration 8 (add user id to messages tables)

--- db/migrations/8_add_user_id_to_message_tables.rb
+++ db/migrations/8_add_user_id_to_message_tables.rb
@@ -1,21 +1,24 @@
 Sequel.migration do
   change do
 
+    add_column :example_bot_messages, :user_id, :Bignum, index: true
+    execute('UPDATE example_bot_messages SET user_id=0')
+
     alter_table :example_bot_messages do
-      add_column :user_id, :Bignum, index: true
-      dataset.from(:example_bot_messages).update(user_id: 0)
-      set_column_allow_null(:user_id, false)
+      set_column_not_null(:user_id)
     end
+
+    add_column :pdr_bot_messages, :user_id, :Bignum, index: true
+    execute('UPDATE pdr_bot_messages SET user_id=0')
 
     alter_table :pdr_bot_messages do
-      add_column :user_id, :Bignum, index: true
-      dataset.from(:pdr_bot_messages).update(user_id: 0)
       set_column_allow_null(:user_id, false)
     end
 
+    add_column :jenia_bot_messages, :user_id, :Bignum, index: true
+    execute('UPDATE jenia_bot_messages SET user_id=0')
+
     alter_table :jenia_bot_messages do
-      add_column :user_id, :Bignum, index: true
-      dataset.from(:jenia_bot_messages).update(user_id: 0)
       set_column_allow_null(:user_id, false)
     end
 
