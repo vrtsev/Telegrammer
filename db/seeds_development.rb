6f33e10f7227f15dfb82c148b4e55d7d29c0d8b1
vrtsev <v.rtsev@gmail.com>
2020-07-28 23:44:01 +0300

    Added development seeds file

---
+++ db/seeds_development.rb
@@ -0,0 +1,6 @@
+# Can be used only for development purposes
+
+# Create test user for PdrBot
+test_user_id = 123_456
+PdrBot::UserRepository.new.create(id: test_user_id, name: 'test_user_1', first_name: 'Test', last_name: 'User 1')
+PdrBot::ChatUserRepository.new.create(user_id: test_user_id, chat_id: ENV['TELEGRAM_APP_OWNER_ID'])
