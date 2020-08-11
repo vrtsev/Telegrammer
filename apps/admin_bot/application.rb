f94d23fada7d06013da60a07c59d91a5012bcc6d
vrtsev <v.rtsev@gmail.com>
2020-08-11 23:47:14 +0300

    Cleanup base application.rb file

--- apps/admin_bot/application.rb
+++ apps/admin_bot/application.rb
@@ -5,46 +5,18 @@ require_relative 'controller.rb'
 
 module AdminBot
   class Application < Telegram::AppManager::Application
-
-    def configure
-      super
-
-      AdminBot::UserRepository.new.find_or_create(ENV['TELEGRAM_APP_OWNER_ID'], {
-        id: ENV['TELEGRAM_APP_OWNER_ID'],
-        role: AdminBot::User::Roles.administrator
-      })
-    end
-
-    def run
-      super
-    rescue HTTPClient::ReceiveTimeoutError => exception
-      puts "Poller timeout error. Reconnecting"
-      run
-    end
-
     private
 
     def controller
       AdminBot::Controller
     end
 
-    def configuration_message
-      # Change config message here
-      super
-    end
-
-    def startup_message
-      # Change startup message here
-      super
-    end
-
     def handle_exception(exception)
-      puts "[#{@configuration.app_name}] Application raised exception...".bold.red
       Telegram::AppManager::Message
         .new(Telegram.bots[:admin_bot], exception.full_message.truncate(4000))
         .send_to_app_owner
-      raise exception
-    end
 
+      super
+    end
   end
 end
--- apps/jenia_bot/application.rb
+++ apps/jenia_bot/application.rb
@@ -4,42 +4,18 @@ require_relative 'controller.rb'
 
 module JeniaBot
   class Application < Telegram::AppManager::Application
-
-    def configure
-      # Configure your app here
-      super
-    end
-
-    def run
-      super
-    rescue HTTPClient::ReceiveTimeoutError => exception
-      puts "Poller timeout error. Reconnecting"
-      run
-    end
-
     private
 
     def controller
       JeniaBot::Controller
     end
 
-    def configuration_message
-      # Change config message here
-      super
-    end
-
-    def startup_message
-      # Change startup message here
-      super
-    end
-
     def handle_exception(exception)
-      puts "[#{@configuration.app_name}] Application raised exception...".bold.red
       Telegram::AppManager::Message
         .new(Telegram.bots[:admin_bot], exception.full_message.truncate(4000))
         .send_to_app_owner
-      raise exception
-    end
 
+      super
+    end
   end
 end
