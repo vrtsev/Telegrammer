d16ed9f27fae7760573054f48c64eeba6dce9607
vrtsev <v.rtsev@gmail.com>
2020-08-12 03:02:43 +0300

    Implement i18n keys consistency validation

--- config/i18n.rb
+++ config/i18n.rb
@@ -1,4 +1,18 @@
 LOCALES_DIR_NAME = 'locales'.freeze
-I18n.load_path << Dir[File.expand_path(LOCALES_DIR_NAME) + '/**/*.yml']
+I18n.load_path = Dir[File.expand_path(LOCALES_DIR_NAME) + '/**/*.yml']
 
-# TODO: Implement keys validation
+# Keys consistency validation
+default_locale = I18n.default_locale.to_s
+available_locales = I18n.available_locales.map(&:to_s)
+
+(available_locales - Array(default_locale)).each do |locale|
+  default_locale_files = Dir[File.expand_path(LOCALES_DIR_NAME) + "/#{default_locale}" + '/*.yml']
+  default_locale_files.each do |default_locale_path|
+    default_file = I18nSpec::LocaleFile.new(default_locale_path)
+    file = I18nSpec::LocaleFile.new(default_locale_path.sub(default_locale, locale))
+
+    @misses = default_file.flattened_translations.keys - file.flattened_translations.keys
+    raise "Error. Missing translation keys in '#{file.filepath}': #{@misses}" unless @misses.empty?
+  rescue Errno::ENOENT
+  end
+end
