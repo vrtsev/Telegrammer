241e41be009a2942e4047f19cfb54f811dd3b605
vrtsev <v.rtsev@gmail.com>
2022-06-11 03:32:59 +0300

    Reordered dependencies loading

--- lib/application.rb
+++ lib/application.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: false
 
-require_all './lib/application'
+require_all './lib/application/helpers'
+require_all './lib/application/models'
+require './lib/application/services/base_service.rb'
+require_all './lib/application/services'
+require_all './lib/application/workers'
 
 module Application
 end
--- lib/telegram/app_manager.rb
+++ lib/telegram/app_manager.rb
@@ -5,26 +5,19 @@ require 'dry-validation'
 require 'logger'
 
 # Require main dependencies
-require_relative 'app_manager/helpers/responding.rb'
-require_relative 'app_manager/helpers/logging.rb'
-require_relative 'app_manager/helpers/validation.rb'
-
-require_relative 'app_manager/configuration.rb'
-require_relative 'app_manager/application.rb'
-require_relative 'app_manager/service.rb'
-require_relative 'app_manager/worker.rb'
 
 require_relative 'app_manager/logger/multi_io.rb'
 require_relative 'app_manager/logger/default_formatter.rb'
 require_relative 'app_manager/logger/active_record_formatter.rb'
 require_relative 'app_manager/logger.rb'
-require_relative 'app_manager/log_subscriber.rb'
 
-require_relative 'app_manager/builder.rb'
-require_relative 'app_manager/builders/chat_user.rb'
-require_relative 'app_manager/builders/chat.rb'
-require_relative 'app_manager/builders/message.rb'
-require_relative 'app_manager/builders/user.rb'
+require_relative 'app_manager/configuration.rb'
+require_relative 'app_manager/application.rb'
+require_relative 'app_manager/template.rb'
+require_relative 'app_manager/controller.rb'
+
+require_relative 'app_manager/extensions/log_subscriber.rb'
+
 module Telegram
   module AppManager
     class << self
